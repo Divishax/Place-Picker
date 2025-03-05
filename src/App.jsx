8d591dfe85455bb64503117be0e27f70747f6d70
Divishax <divishagandhi@gmail.com>
2025-03-05 13:32:16 +0530

    changing the modal from Imperitive way to Declarative way

--- src/App.jsx
+++ src/App.jsx
@@ -13,8 +13,8 @@ const storedPlaces = storedIds
   .filter((place) => place !== undefined);
 
 function App() {
-  const modal = useRef();
   const selectedPlace = useRef();
+  const [modalIsOPen, setModalIsOpen] = useState(false);
   const [availablePlaces, setAvalilablePlaces] = useState([]);
   const [pickedPlaces, setPickedPlaces] = useState([...storedPlaces]);
 
@@ -45,12 +45,12 @@ function App() {
   // });
 
   function handleStartRemovePlace(id) {
-    modal.current.open();
+    setModalIsOpen(true);
     selectedPlace.current = id;
   }
 
   function handleStopRemovePlace() {
-    modal.current.close();
+    setModalIsOpen(false);
   }
 
   function handleSelectPlace(id) {
@@ -76,7 +76,7 @@ function App() {
     setPickedPlaces((prevPickedPlaces) =>
       prevPickedPlaces.filter((place) => place.id !== selectedPlace.current)
     );
-    modal.current.close();
+    setModalIsOpen(false);
 
     // Another Side Effect
     const storedIds = JSON.parse(localStorage.getItem("selectedPlaces")) || [];
@@ -88,7 +88,7 @@ function App() {
 
   return (
     <>
-      <Modal ref={modal}>
+      <Modal open={modalIsOPen}>
         <DeleteConfirmation
           onCancel={handleStopRemovePlace}
           onConfirm={handleRemovePlace}
